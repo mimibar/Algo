3378bda996aabb033ecb3a6ba0ab0b4f0ecc138b
Miriam Lee <mimibaari+githubmimiAsusIII@gmail.com>
2016-09-03 16:56:47 -0500

    [PASSED] Week 6 Quiz: Programming Assignment #6
    Passed
    2/2 points earned (100%)
    Quiz passed!

---
+++ Part 1/Week 6/src/Median.java
@@ -0,0 +1,89 @@
+/**
+ * @name Miriam Lee
+ * @course Algorithms: Design and Analysis, Part 1
+ * @date Sep 3, 2016 3:02:54 PM
+ * @question The goal of this problem is to implement the "Median Maintenance" algorithm (covered in
+ *           the Week 5 lecture on heap applications).
+ * @score 2/2 points earned (100%)
+ */
+import java.io.File;
+import java.util.Scanner;
+
+import edu.princeton.cs.algs4.MaxPQ;
+import edu.princeton.cs.algs4.MinPQ;
+
+/**
+ * @author mimi
+ *
+ */
+public class Median {
+
+  private static int k;
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    long ti = System.currentTimeMillis();
+    MaxPQ<Integer> hLow = new MaxPQ<Integer>();
+    MinPQ<Integer> hHigh = new MinPQ<Integer>();
+    Long ans = 0L;
+    Scanner in;
+    try {
+      in = new Scanner(new File("bin/_6ec67df2804ff4b58ab21c12edcb21f8_Median.txt"));
+      // in = new Scanner(System.in);
+      while (in.hasNext()) {
+
+        int x = in.nextInt();
+        k++;
+        // So, if k is odd, then mk is ((k+1)/2)th smallest number among x1,…,xk; if k is even, then
+        // mk is the (k/2)th smallest number among x1,…,xk.)
+        if (k == 1 || x < hLow.max()) {
+          if (k > 1 && k % 2 == 0)
+            hHigh.insert(hLow.delMax());
+          hLow.insert(x);
+        } else {
+          hHigh.insert(x);
+          if (k > 1 && k % 2 != 0)
+            hLow.insert(hHigh.delMin());
+        }
+
+        System.out.println(hLow.max());
+        ans += hLow.max();
+      }
+      System.out.println("Answer: " + ans + " mod " + (ans % 10000) + " in "
+          + (System.currentTimeMillis() - ti) / 1000.0 + "s");// Answer: 46831213 mod 1213 in 0.209s
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  // For PA2: head Median.txt > testMedian.txt
+  // 6331
+  // 2793
+  // 1640
+  // 9290
+  // 225
+  // 625
+  // 6195
+  // 2303
+  // 5685
+  // 1354
+  // Answer 9335
+  //
+  // My output
+  // 6331 Median is 6331
+  // 2793 Median is 2793
+  // 1640 Median is 2793
+  // 9290 Median is 2793
+  // 225 Median is 2793
+  // 625 Median is 1640
+  // 6195 Median is 2793
+  // 2303 Median is 2303
+  // 5685 Median is 2793
+  // 1354 Median is 2303
+  // The last 4 digits of the average are 9335
+
+}
--- Part 1/Week 6/src/TwoSum.java
+++ Part 1/Week 6/src/TwoSum.java
@@ -4,14 +4,15 @@
  * @date Sep 2, 2016 10:33:39 PM
  * @question The goal of this problem is to implement a variant of the 2-SUM algorithm (covered in
  *           the Week 6 lecture on hash table applications).
- * @score
+ * @score 2/2 points earned (100%)
  */
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.time.DateTimeException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 /**
  * @author mimi
@@ -23,39 +24,55 @@ public class TwoSum {
    * @param args
    */
   public static void main(String[] args) {
-    // The file contains 1 million integers, both positive and negative (there might be some
-    // repetitions!)
-    // Scanner in = new Scanner(System.in); //
-    long t = System.currentTimeMillis();
+    long ti = System.currentTimeMillis();
     Scanner in;
     try {
-      // in = new Scanner(
-      // new File("bin/_6ec67df2804ff4b58ab21c12edcb21f8_algo1-programming_prob-2sum.txt"));
-      in = new Scanner(System.in);
+      // The file contains 1 million integers, both positive and negative (there might be some
+      // repetitions!)
+      in = new Scanner(
+          new File("bin/_6ec67df2804ff4b58ab21c12edcb21f8_algo1-programming_prob-2sum.txt"));
+      // in = new Scanner(System.in);
       int ans = 0;
       HashSet<Long> H = new HashSet<>();
+      ArrayList<Long> A = new ArrayList<>();
+
       // 1.) insert elements of A into hash table H
       while (in.hasNext()) {
-
-        H.add(in.nextLong());
+        long a = in.nextLong();
+        if (!H.contains(a))
+          A.add(a);
+        H.add(a);
       }
-      // 2.) for each x in A, Lookup t-x
-      Iterator<Long> it = H.iterator();
-      while (it.hasNext()) {
-        Long x = it.next();
-        for (long y = -10000 - x; y < 10000 - x; y++) {
-          if (y == 0)
-            continue;
-          if (x != y && H.contains(y)) {// TODO (NOTE: ensuring distinctness requires a one-line
-                                        // addition to the algorithm from lecture.)
+      Collections.sort(A);
+      TreeSet<Integer> ts = new TreeSet<Integer>();
+      for (int t = -10000; t <= 10000; t++)
+        ts.add(t);
+
+      for (int i = 0; i < A.size(); i++) {
+        // 2.) for each x in A, Lookup t-x
+        Long x = A.get(i);
+        Iterator<Integer> it = ts.iterator();
+        while (it.hasNext()) {
+          Integer t = it.next();
+
+          long y = t - x;
+          // TODO (NOTE: ensuring distinctness requires a one-line addition to the algorithm from
+          // lecture.)
+          if (x != y && y < x && H.contains(y)) {
             ans++;
-            System.out.println("( " + x + "," + y + ") ");
+            System.out.println("(" + x + " ," + y + ")");
+            it.remove();
           }
         }
       }
-      System.out.println(
-          "Answer: " + ans / 2 + " in " + (System.currentTimeMillis() - t) / 1000.0 + "ms");// 50195
-    } catch (Exception e) {
+
+      // (an integer between 0 and 20001)
+      System.out
+          .println("Answer: " + ans + " in " + (System.currentTimeMillis() - ti) / 1000.0 + "s");// 50195
+    } // Answer: 427 in 661.104s
+    catch (
+
+    Exception e) {
       e.printStackTrace();
     }
   }
